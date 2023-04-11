fdb737a0380d4343318fbd8ef85108632d31726c
Vioneta-Zarazsofia <vioneta0730@gmail.com>
2023-04-11 19:36:18 +0700

    first commit

---
+++ Praktikum 1/latihan_4.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Pemrograman PHP dengan Array</title>
+    </head>
+    <body>
+        <?php
+        echo date ("m-F-Y, g:i:s a");
+        ?>
+    </body>
+</html>
